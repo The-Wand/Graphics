cc6abdb5ab3d89c33a84c3e636a8480caf4fd2b0
Alex Kozin <al@el-machine.com>
2025-09-24 14:38:38 +0700

    Update auto test

--- Tests/WandTests/Unit/XCTestCase.swift
+++ Tests/WandTests/Unit/XCTestCase.swift
@@ -1,13 +1,11 @@
 ///
-/// Copyright © 2020-2024 El Machine 🤖
-/// https://el-machine.com/
+/// Copyright 2020 Alexander Kozin
 ///
 /// Licensed under the Apache License, Version 2.0 (the "License");
 /// you may not use this file except in compliance with the License.
 /// You may obtain a copy of the License at
 ///
-/// 1) LICENSE file
-/// 2) https://apache.org/licenses/LICENSE-2.0
+///     http://www.apache.org/licenses/LICENSE-2.0
 ///
 /// Unless required by applicable law or agreed to in writing, software
 /// distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,12 +14,10 @@
 /// limitations under the License.
 ///
 /// Created by Alex Kozin
-/// 2020 El Machine
-
-import Foundation
-import XCTest
+/// El Machine 🤖
 
 import Wand
+import XCTest
 
 /// Asking
 extension XCTestCase {
@@ -31,8 +27,8 @@ extension XCTestCase {
     }
 
     func auto<C, T: Asking>(test context: C,
-                               api:   ( (C, (T)->() ) )->(Wand) ,
-                               completion:  @escaping (T)->() ) {
+                            api: ( (C, (T)->()) )->(Core),
+                            completion:  @escaping (T)->() ) {
 
         let e = expectation()
         e.assertForOverFulfill = true
@@ -43,19 +39,20 @@ extension XCTestCase {
         }) )
 
         waitForExpectations(timeout: .default)
+
     }
 
 }
 
 /// AskingNil
 extension XCTestCase {
-    
+
     func auto_test<T: AskingNil>(completion:  @escaping (T)->() ) {
         auto_test(|, completion: completion)
     }
 
-    func auto_test<T: AskingNil>(_ api:   ( @escaping (T)->() )->(Wand) ,
-                               completion:  @escaping (T)->() ) {
+    func auto_test<T: AskingNil>(_ api:   ( @escaping (T)->() )->(Core) ,
+                                 completion:  @escaping (T)->() ) {
 
         let e = expectation()
         e.assertForOverFulfill = true
@@ -66,6 +63,7 @@ extension XCTestCase {
         })
 
         waitForExpectations(timeout: .default)
+
     }
 
 }
